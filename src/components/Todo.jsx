1e964c8ef508b394ca330c89bd2dc6609beb6c60
yash <yash9570raj@gmail.com>
2024-09-15 23:45:31 +0530

    new pages added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import todo_icon from "../assets/todo_icon.png";
 import Todoitems from "./Todoitems";
 
-
 const Todo = () => {
+  const [todoList, setTodoList] = useState([]);
+  const inputRef = useRef();
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+
+    if (inputText === "") {
+      return null;
+    }
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isCompleted: false,
+    };
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
       {/* {-----------title-------} */}
@@ -17,17 +32,23 @@ const Todo = () => {
       {/* {-----------input-------} */}
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
         <input
+          ref={inputRef}
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
           type="text"
           placeholder="Add a new task"
         />
-        <button className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">
+        <button
+          onClick={add}
+          className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+        >
           Add +
         </button>
       </div>
       {/* {-----------todo list-------} */}
       <div>
-        <Todoitems />
+        {todoList.map((item, index) => {
+          return <Todoitems key={index} text={item.text} />;
+        })}
       </div>
     </div>
   );
